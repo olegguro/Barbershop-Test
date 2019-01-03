e3ce4a7127ee93da7a75f6dfbbbca4461e6a28b7
Oleg Guro <ooleg6334@gmail.com>
2019-01-03 11:08:37 +0300

    # Add zone /admin where we can see list of users from users.txt (as admin)

--- barbershop.rb
+++ barbershop.rb
@@ -4,6 +4,7 @@ get '/' do
    	erb :index
 end
 
+# Asking name, phone, date
 post '/' do
 	@user_name=params[:user_name]
 	@phone=params[:phone]
@@ -11,11 +12,32 @@ post '/' do
 	
 	@title='Thank you!'
 	@message="Dear #{@user_name}, we'll be waiting you at #{@date_time}"
-
-	f= File.open './public/user.txt', 'a'
-	f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}"
+       
+#write information into user.txt 
+	f= File.open './public/users.txt', 'a'
+	f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}\n" 
 	f.close
 
 	erb :message
 end
 
+# Add zone /admin where we can see list of users from users.txt (as admin) 
+# sinatra text file sso
+get '/admin' do
+	erb :admin
+end
+
+post '/admin' do
+	@login = params[:login]
+	@password = params[:password]
+
+	# checking login and password, if true you enter else not
+	if @login == 'admin' && @password == 'oleg'
+  	@file = File.open("./public/users.txt","r")
+  	erb :watch_result
+	  # @file.close - должно быть, но тогда не работает. указал в erb
+	else
+		@report = '<p>Access is denied! Wrong login or password.</p>'
+		erb :admin
+	end
+end
